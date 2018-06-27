6a56c65901f5ed7fce4fa3d0b7990598f25b9773
Nguyen Ba Hung <bahung1221@gmail.com>
2018-06-27 14:01:21 +0700

    Init

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "sourceType": "module",
+        "ecmaVersion": 2018
+    },
+    "rules": {
+        "indent": [
+            "error",
+            2
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "never"
+        ],
+        "no-unused-vars": 0,
+        "experimentalObjectRestSpread": true,
+        "no-console": 0
+    }
+}
---
+++ readme.md
@@ -0,0 +1,1 @@
+# Sample nodejs project structure
