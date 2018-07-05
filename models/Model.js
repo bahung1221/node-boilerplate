5568fb8377b5d4ee39c051a3192df6c6494f4730
Nguyen Ba Hung <bahung1221@gmail.com>
2018-07-05 18:03:53 +0700

    Add some boilerplate for base model

--- models/Model.js
+++ models/Model.js
@@ -3,24 +3,40 @@ const DBManage = require('../database/DBManage')
 
 const Model = function (table, sql) {
   this._table = table
+}
 
-  this.index = function (options) {
-    let self = this
+Model.prototype.index = function (options) {
+  let self = this
 
-    return new Promise(function(resolve, reject) {
-      let queryString = queryBuilder.buildQueryString(options),
-        sql = `SELECT * from ${self._table} ${queryString}`
+  return new Promise(function(resolve, reject) {
+    let queryString = queryBuilder.buildQueryString(options),
+      sql = `SELECT * from ${self._table} ${queryString}`
 
-      DBManage.executeQuery(sql, function(err, data) {
-        if (err) {
-          console.log(err.message)
-          reject(err)
-        }
+    DBManage.executeQuery(sql, function(err, data) {
+      if (err) {
+        console.log(err.message)
+        reject(err)
+      }
 
-        resolve(data)
-      })
+      resolve(data)
     })
-  }
+  })
+}
+
+Model.prototype.find = function (id) {
+  // TODO
+}
+
+Model.prototype.store = function (data) {
+  // TODO
+}
+
+Model.prototype.update = function (id, data) {
+  // TODO
+}
+
+Model.prototype.delete = function (id) {
+  // TODO
 }
 
 module.exports = {
