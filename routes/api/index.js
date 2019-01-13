fd7538da0d86dba77a1dcb24d2860c0c821b12bd
bahung1221 <bahung1221@gmail.com>
2019-01-13 13:15:27 +0700

    Switch to express

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,31 +1,9 @@
 const express = require('express')
 const router = express.Router()
-const { Model } = require('../../models/Model')
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
-  let model = new Model('test')
-  // let response = await model.store({
-  //   name: 'test ne',
-  //   catalog_id: 1,
-  //   content: 'Test content ne'
-  // })
-
-  // let response = await model.find(2)
-  let response = await model.index(
-    {
-      limit: 10,
-      page: 2,
-      sort: '-id'
-    },
-    true
-  )
-  // let response = await model.delete(2)
-  // let response = await model.update(2, {
-  //   name: 'update 2'
-  // })
-  // res.send('GET "/api" OK')
-  res.json(response)
+  res.send('GET "/api" OK')
 })
 
 module.exports = router
